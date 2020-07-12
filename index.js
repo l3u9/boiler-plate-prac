228c8656d211f1e7cc4199c4f3945f59655ece34
l3u9 <ksj31292000@gmail.com>
2020-07-13 03:49:01 +0900

    add

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+//application.x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://13u9:1023@boiler-plate.wzbha.mongodb.net/<dbname>?retryWrites=true&w=majority',
 {
@@ -11,5 +20,21 @@ mongoose.connect('mongodb+srv://13u9:1023@boiler-plate.wzbha.mongodb.net/<dbname
 
 
 app.get('/', (req, res) => res.send('Hello World!~~안녕하세요 ~ '))
+
+
+app.post('/register', (req, res) => {
+    //회원가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 게이터 베이스에 넣어준다
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
