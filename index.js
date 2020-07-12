bafd6754c6adb33998d558fab3cf4439849f9beb
l3u9 <ksj31292000@gmail.com>
2020-07-13 04:01:01 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser')
+
+const config = require('./config/key')
+
 const { User } = require("./models/User")
 
 //application.x-www-form-urlencoded
@@ -12,14 +15,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://13u9:1023@boiler-plate.wzbha.mongodb.net/<dbname>?retryWrites=true&w=majority',
+mongoose.connect(config.mongoURI,
 {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!~~안녕하세요 ~ '))
+app.get('/', (req, res) => res.send('Hello World!~~ ~ '))
 
 
 app.post('/register', (req, res) => {
